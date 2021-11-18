1144977f3f434cbc3f166a45a2dc1bfa96f57782
coocooooo <yuzhou.ini@outlook.com>
2021-11-18 14:00:02 +0800

    更换代码主题色

--- site/gatsby-browser.js
+++ site/gatsby-browser.js
@@ -2,8 +2,10 @@
 import '@fontsource/nunito';
 // normalize CSS across browsers
 import './src/normalize.css';
-// custom CSS styles
-import './src/style.css';
 
 // Highlighting for code blocks
-import 'prismjs/themes/prism.css';
+import 'prism-themes/themes/prism-one-light.css';
+// import 'prism-themes/themes/prism-coy-without-shadows.css';
+
+// custom CSS styles
+import './src/style.css';
